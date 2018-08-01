59dec86c8e6763bd3c76261095ed510174fed98e
Shu <ryhshu@gmail.com>
2018-08-01 14:47:41 -0400

    updates to objects and arrays

---
+++ objects/var.js
@@ -0,0 +1,16 @@
+// var firstName = 'Shu'
+
+// if (10 === 10) {
+//     let firstName = 'Jen'
+// }
+
+// console.log(firstName)
+
+// const setName = function() {
+//     var firstName = 'Jen'
+// }
+
+// setName()
+// console.log(firstName)
+
+let
