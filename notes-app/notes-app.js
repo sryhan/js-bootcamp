3cc8233412e22318c3d26cea0e77fdd4973e08d2
Shu <sryhan8520@gmail.com>
2018-08-04 23:29:06 -0400

    createElement and appendChild

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -1,3 +1,14 @@
+const notes = [{
+    title: 'My next trip', 
+    body: 'I would like to go to Japan'
+}, {
+    title: 'Habits to work on',
+    body: 'Exercise more. Diet.'
+}, {
+    title: 'Study',
+    body: 'JavaScript, Java, Python, Django, Photoshop, Vue, React, etc.'
+}]
+
 // DOM - Document Object Model 
 
 // Query and remove 
@@ -11,3 +22,8 @@ ps.forEach(function (p) {
     // console.log(p.textContent)
     // p.remove()
 })
+
+// Add a new element 
+const newParagraph = document.createElement('p')
+newParagraph.textContent = 'This is a new element from JavaScript'
+document.querySelector('body').appendChild(newParagraph)
--- todo-app/todo-app.js
+++ todo-app/todo-app.js
@@ -1,8 +1,43 @@
-const p = document.querySelectorAll('p')
+const todos = [{
+    text: 'Exercise',
+    completed: true
+}, {
+    text: 'Practice Javascript',
+    completed: true
+}, {
+    text: 'Practice Java',
+    completed: false 
+}, {
+    text: 'Practice Web Development',
+    completed: false
+}]
 
-p.forEach(function(p) {
-    if (p.textContent.includes('four')) {
-        p.remove()
-    }
-    // console.log(p.textContent)
+const incompleteTodos = todos.filter(function (todo) {
+    return !todo.completed
 })
+
+const summary = document.createElement('h2')
+summary.textContent = `You have ${incompleteTodos.length} todos left`
+document.querySelector('body').appendChild(summary)
+
+todos.forEach(function(todo) {
+    if (!todo.completed) {
+        const newPara = document.createElement('p')
+        newPara.textContent = `You have not completed ${todo.text}`
+        document.querySelector('body').appendChild(newPara)
+    }
+})
+
+
+
+
+
+
+// const p = document.querySelectorAll('p')
+
+// p.forEach(function(paragraph) {
+//     if (paragraph.textContent.includes('four')) {
+//         paragraph.remove()
+//     }
+//     // console.log(p.textContent)
+// })
