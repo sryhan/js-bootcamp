f111c249b238d64465643ecb4c5d42930be196e2
Shu <ryhshu@gmail.com>
2018-08-06 06:57:30 -0400

    update to todos

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -9,6 +9,49 @@ const notes = [{
     body: 'JavaScript, Java, Python, Django, Photoshop, Vue, React, etc.'
 }]
 
+const filters = {
+    searchText: ''
+}
+
+const renderNotes = function(notes, filters) {
+    const filteredNotes = notes.filter(function(note) {
+        return note.title.toLowerCase().includes(filters.searchText.toLowerCase())
+    })
+
+    document.querySelector('#notes').innerHTML = ''
+    
+    filteredNotes.forEach(function (note) {
+        const noteEl = document.createElement('p')
+        noteEl.textContent = note.title 
+        document.querySelector('#notes').appendChild(noteEl)
+    })
+}
+
+renderNotes(notes, filters)
+
+document.querySelector('#create-note').addEventListener('click', function(e) {
+    // console.log('Did this work?')
+    // console.log(e)
+    e.target.textContent = 'The button was clicked'
+})
+
+document.querySelector('#search-text').addEventListener('input', function(e) {
+    // console.log(e.target.value)
+    filters.searchText = e.target.value
+    renderNotes(notes, filters)
+})
+
+document.querySelector('#for-fun').addEventListener('change', function(e) {
+    e.target.checked
+})
+
+
+// document.querySelector('#name-form').addEventListener('submit', function(e) {
+//     e.preventDefault()
+//     console.log(e.target.elements.firstName.value)
+//     e.target.elements.firstName.value = ''
+// })
+
 // DOM - Document Object Model 
 
 // Query and remove 
@@ -16,14 +59,14 @@ const notes = [{
 // p.remove()
 
 // Query all and remove 
-const ps = document.querySelectorAll('p')
-ps.forEach(function (p) {
-    p.textContent = '*******'
-    // console.log(p.textContent)
-    // p.remove()
-})
+// const ps = document.querySelectorAll('p')
+// ps.forEach(function (p) {
+//     p.textContent = '*******'
+//     // console.log(p.textContent)
+//     // p.remove()
+// })
 
 // Add a new element 
-const newParagraph = document.createElement('p')
-newParagraph.textContent = 'This is a new element from JavaScript'
-document.querySelector('body').appendChild(newParagraph)
+// const newParagraph = document.createElement('p')
+// newParagraph.textContent = 'This is a new element from JavaScript'
+// document.querySelector('body').appendChild(newParagraph)
--- todo-app/todo-app.js
+++ todo-app/todo-app.js
@@ -12,22 +12,71 @@ const todos = [{
     completed: false
 }]
 
-const incompleteTodos = todos.filter(function (todo) {
-    return !todo.completed
+
+const filters = {
+    searchText: ''
+}
+
+const renderTodos = function(todos, filters) {
+    const filteredTodos = todos.filter(function (todo) {
+        return todo.text.toLowerCase().includes(filters.searchText.toLowerCase())
+    })
+
+    const incompleteTodos = filteredTodos.filter(function (todo) {
+        return !todo.completed
+    })
+
+    document.querySelector('#todos-left').innerHTML = ''
+    
+    const summary = document.createElement('h2')
+    summary.textContent = `You have ${incompleteTodos.length} todos left`
+    document.querySelector('#todos-left').appendChild(summary)
+    
+    filteredTodos.forEach(function(todo) {
+        if (!todo.completed) {
+            const newPara = document.createElement('p')
+            newPara.textContent = `${todo.text}`
+            document.querySelector('#todos-left').appendChild(newPara)
+        }
+    })
+}
+
+renderTodos(todos, filters)
+
+document.querySelector('#filter-todos').addEventListener('input', function(e){
+    filters.searchText = e.target.value
+    renderTodos(todos, filters)
 })
 
-const summary = document.createElement('h2')
-summary.textContent = `You have ${incompleteTodos.length} todos left`
-document.querySelector('body').appendChild(summary)
-
-todos.forEach(function(todo) {
-    if (!todo.completed) {
-        const newPara = document.createElement('p')
-        newPara.textContent = `You have not completed ${todo.text}`
-        document.querySelector('body').appendChild(newPara)
-    }
+document.querySelector('#todo-form').addEventListener('submit', function(e) {
+    e.preventDefault()
+    todos.push({
+        text: e.target.elements.thisTodo.value,
+        completed: false
+    })
+    renderTodos(todos, filters)
+    e.target.elements.thisTodo.value = ''
 })
 
+document.querySelector('#todo-checkbox').addEventListener('change', function(e) {
+    e.target.checked
+})
+
+
+// 1. Create a checkbox and setup event listener -> "hide completed"
+// 2. Create new hideCompleted filter (default false)
+// 3. Update hideCompleted and rerender list on checkbox change
+// 4. Setup renderTodos to remove completed items 
+
+// Listen for new todo creation 
+// document.querySelector('#add-todo').addEventListener('click', function(e) {
+//     console.log('You clicked the button!')
+// })
+
+// List for todo text change
+// document.querySelector('#new-todo').addEventListener('input', function(e) {
+//     console.log(e.target.value)
+// })
 
 
 
